d3c9684dd15acb7a930b007e510ed764f97d54e7
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-07-24 21:51:48 +0530

    microservices communication via RestClient

--- customer/src/main/java/com/tigercodes/customer/CustomerService.java
+++ customer/src/main/java/com/tigercodes/customer/CustomerService.java
@@ -1,9 +1,11 @@
 package com.tigercodes.customer;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public record CustomerService(CustomerRepository customerRepository) {
+public record CustomerService(CustomerRepository customerRepository,
+                              RestTemplate restTemplate) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
                 .firstName(request.firstName())
@@ -14,6 +16,19 @@ public record CustomerService(CustomerRepository customerRepository) {
         //todo: check if email is valid
         //todo: check if email is not taken
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        //todo: check if customer is fraudster
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.isFraudster()) {
+            throw new IllegalStateException("fraudster");
+        }
+
+        //todo: send notification
     }
 }
